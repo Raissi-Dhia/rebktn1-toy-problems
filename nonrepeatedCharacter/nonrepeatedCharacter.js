e4046f5843d90bd5fdadf44fcadec96430956ef7
Raissi-Dhia <estraissi@gmail.com>
2019-11-25 10:14:51 +0100

    toy problem

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,16 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
-
+// we are going to split and itterate than check if it is there twice or not if yes return it if not just doesn't return any value 
+// couldn't solve it in tim and i couldn't comment in details cauz this is  commented in the last min 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var result = null ; 
+  result = string.split('')
+  for(var i =0; i < arr.length; i++){
+  if(string.indexOf(string[i]) === i && string.indexOf(string[i++]) === -1 ){
+  	result = string[]; 
+  }
+  }
+  return result ; 
 };
