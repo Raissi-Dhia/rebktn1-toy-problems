9605c0e4ba11e6d3f5250e3a484c7923e1e7b6a2
Raissi-Dhia <estraissi@gmail.com>
2019-12-03 09:15:51 +0100

    Toy problem

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,7 +9,27 @@
  * var index = binarySearch([1, 2, 3, 4, 5], 8);
  * console.log(index); // null
  */
+ // logicaly i think it works like this but i don't know what it is missing exactly ,
+ // all what we have to here is to keep spliting the array and check if the target is bigger or smaller than the mid number so we can know how which array we ll keep looking in either left or right and each time we get rid of the half array that we don't want to use and use the recursion here to check each time and do the same work ,
+ //untill we get to it else w e return null . 
 
 var binarySearch = function (array, target) {
+ 	var mid = array.length / 2 ; 
+ 		
+ 		if (array[0] === target){
+ 			return array[0];
+ 		}else if (array[array.length-1] === target){
+ 			return array.length-1;
+ 		}else if (array[mid] === target){
+ 			return mid ;
+ 		}else if (array[mid] > target){
+ 			let newArr = array.splice(0, array[mid])
+ 			return binarySearch(newArr , target); 
+ 		}else if (array[mid] < target){
+ 			let newArr = array.splice(array[mid],array.length)
+ 			return binarySearch(newArr , target); 
+ 		}else{
+ 			return null;
+ 		}
 };
 
