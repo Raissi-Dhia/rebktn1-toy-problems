dedc3875fca1a40934f3da522eb4ca6f3ceebc67
Raissi-Dhia <estraissi@gmail.com>
2020-01-16 09:00:00 +0100

    toy problem

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,4 +9,11 @@ primeTester(1) // false
 
 */
 
-function primeTester(n) {}
+function primeTester(n) {
+    for (var i = 0; i < n; i++ ){
+        if (n % i === 0){
+            return false 
+        }
+    }
+    return true;
+}
